c8af27fc27f2755d6a2e43e95c5b64dc8af08485
Joe G <Joseph.gashi86@gmail.com>
2022-10-05 05:41:09 -0400

    changes after CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
 import Employee from "./Components/Employee";
-import { useState } from "react";
+
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("Dev");
+  const [employee, setEmployees] = useState([
+    {
+      name: "Hetheo",
+      role: "Developer",
+      img:
+        "https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+    {
+      name: "Aries",
+      role: "Developer",
+      img:
+        "https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+    {
+      name: "Sephiroth",
+      role: "Developer",
+      img:
+        "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+  ]);
 
   return (
     <div className="App">
@@ -14,9 +36,19 @@ function App() {
           setRole(e.target.value);
         }}
       ></input>
-      <Employee name="Natalie" role={role} />
-      <Employee name="Sofia" role="Creator" />
-      <Employee name="Amanda" role="Managment" />
+      <div>
+        {employee.map((employee) => {
+          return (
+            <Employee
+              key={uuidv4()}
+              name={employee.name}
+              role={employee.role}
+              img={employee.img}
+              alt="people"
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -2,9 +2,17 @@ import React from "react";
 
 export default function Employee(props) {
   return (
-    <>
-      <div>Employee{props.name}</div>
-      <p>{props.role}</p>
-    </>
+    <div className="container">
+      <div className="card">
+        <div>
+          <img className="pic" src={props.img} alt="people"></img>
+        </div>
+        <div className="in-div">
+          <div className="header">{props.name}</div>
+          <p>{props.role}</p>
+          <button className="btn">Message</button>
+        </div>
+      </div>
+    </div>
   );
 }
